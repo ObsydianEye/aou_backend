19312ea3cb8fe5f622e105ed85644819aeaa8ee1
ObsydianEye <artnectdev@gmail.com>
2025-07-15 21:42:09 +0530

    new intial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const startServer = () => {
+  try {
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  } catch (error) {
+    console.log("Failed to start the server on port 5000");
+    console.log("Error", error);
+  }
+};
+
+startServer();
