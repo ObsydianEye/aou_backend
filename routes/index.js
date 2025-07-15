c4c00665afac0fb378d86a6db6b41d3bdc9c909c
ObsydianEye <artnectdev@gmail.com>
2025-07-15 22:04:25 +0530

    test env var

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,12 @@ router.get("/about", (req, res) => {
 });
 
 router.get("/testenv", (req, res) => {
-  res.send(process.env.TESTVAR);
+  const envVar = process.env.TESTVAR;
+  const message = {
+    msg: "good message",
+    message: `This is a test message from ${process.env.NODE_ENV} environment with value ${envVar}`,
+  };
+  res.json(message);
 });
 
 router.post("/contact", (req, res) => {
