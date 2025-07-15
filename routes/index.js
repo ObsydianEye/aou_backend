d061dc7a36ad99262063cda2e61849572c79c79f
ObsydianEye <artnectdev@gmail.com>
2025-07-15 22:01:11 +0530

    testing env

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ router.get("/about", (req, res) => {
   res.send("This is the About Page");
 });
 
+router.get("/testenv", (req, res) => {
+  res.send(process.env.TESTVAR);
+});
+
 router.post("/contact", (req, res) => {
   res.json({ message: "Contact data received", data: req.body });
 });
